8b1336b5afba219a9d10d9ba36e964bf06d21ce0
Octowl <alsaff1987@gmail.com>
2016-05-03 11:59:27 -0400

    Modify /users/:id to now also show pages by user

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,41 @@
 'use strict';
 var express = require('express');
+var Promise = require('bluebird');
+
 var Page = require('../models').Page;
 var User = require('../models').User;
 
 var usersRouter = express.Router();
 
-usersRouter.get('/', function(req, res, next){
+usersRouter.get('/', function (req, res, next) {
     User.findAll().then(
-        function(users){
-            res.render('users', {users: users});
+        function (users) {
+            res.render('users', {
+                users: users
+            });
         });
 });
 
-usersRouter.get('/:id', function(req, res, next){
-    User.findOne({
+usersRouter.get('/:id', function (req, res, next) {
+    var id = req.params.id
+
+    var userPromise = User.findById(id)
+
+    var pagesPromise = Page.findAll({
         where: {
-            id: req.params.id
+            authorId: id
         }
-    }).then(
-        function(user){
-            res.render('userpage', {user: user});
+    })
+
+    Promise.all([userPromise, pagesPromise]).then(
+        function (values) {
+            var user = values[0];
+            var pages = values[1];
+            res.render('userpage', {
+                user: user,
+                pages: pages
+            });
         }
-    )
+    ).catch(next);
 });
 module.exports = usersRouter;
